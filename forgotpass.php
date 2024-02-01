93d9d4aba73c974ab6d43a1e3879c274930112f3
Sohil Vora <sohilVora2000@gmail.com>
2024-02-01 16:43:48 +0530

    forgot password added through gmail

--- forgotpass.php
+++ forgotpass.php
@@ -1,3 +1,59 @@
+<?php
+require("dbconnect.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if ($_POST) {
+    $u_email = $_POST['email'];
+
+    $q = mysqli_query($con, "select * from register where u_email='{$u_email}'");
+    $count = mysqli_num_rows($q);
+
+    if ($count == 1) {
+        $row = mysqli_fetch_array($q);
+        $msg = "Your Password is::" . $row['u_password'];
+
+
+        //Load Composer's autoloader
+        require 'vendor/autoload.php';
+
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'sohilvora2000@gmail.com';                     //SMTP username
+            $mail->Password   = 'pxyydfhfkevqvffp';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('sohilvora2000@gmail.com', 'Sohil Vora');
+            $mail->addAddress($u_email, 'Sohil Vora');     //Add a recipient
+
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Forgot Password';
+            $mail->Body    = $msg;
+            $mail->AltBody = $msg;
+
+            $mail->send();
+            echo "<script>alert('Password has been sent in your Email'); window.location='index.php';</script>";
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else {
+        echo "<script>alert('Email not found');</script>";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,21 +70,12 @@
             <h1>Forgot Passsword</h1>
             <br><br>
             <div class="form-group">
-                <label for="exampleInputPassword1">Old Password</label>
-                <input type="password" class="form-control" name="ops" id="exampleInputPassword1" placeholder="Password" required>
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
             </div>
             <br>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" name="nps" id="exampleInputPassword1" placeholder="Password" required>
-            </div>
-            <br>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" name="cps" id="exampleInputPassword1" placeholder="Password" required>
-            </div>
-            <br>
-            <button type="submit" class="btn btn-primary">Change Password</button>
+            <a href="index.php" class="btn btn-info">Home</a>
+            <button type="submit" class="btn btn-primary">Send</button>
         </form>
     </div>
 </body>
